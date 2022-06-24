08113135a4b511824a657b09bd709e1e05032afd
unknown <henriquealbino1@gmail.com>
2022-06-24 10:22:24 -0300

    Cancelando e Reativando Assinatura

--- app/Http/Controllers/Subscription/SubscriptionController.php
+++ app/Http/Controllers/Subscription/SubscriptionController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\Subscription;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth;
+
 
 class SubscriptionController extends Controller
 {
@@ -33,7 +34,35 @@ class SubscriptionController extends Controller
 
     public function premium()
     {
-        
+
         return view('subscriptions.premium');
+    }
+
+    public function account()
+    {
+        $invoices = auth()->user()->invoices();
+        return view('subscriptions.account', compact('invoices'));
+    }
+
+    public function downloadInvoice($invoiceId)
+    {
+
+        return Auth::user()->
+                    downloadInvoice($invoiceId,[
+                        'vendor' => config('app,name'),
+                        'product' => 'Assinatura VIP'
+                    ]);
+    }
+
+    public function cancel()
+    {
+        auth()->user()->subscription('default')->cancel();
+        return redirect()->route('subscriptions.account');
+    }
+
+    public function resume()
+    {
+        auth()->user()->subscription('default')->resume();
+        return redirect()->route('subscriptions.account');
     }
 }
